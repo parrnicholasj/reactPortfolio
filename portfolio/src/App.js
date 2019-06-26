9eeb54fb4ddb458729f2ed36562ee8be7d594599
Nicholas Parr <parrnicholasj@gmail.com>
2019-06-26 15:54:38 -0400

    working on navbar

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './components/NavBar'
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+    <NavBar />
+
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class NavBar extends Component
+{
+
+  render()
+  {
+    return (
+      <>
+        <div className="row">
+          <div className="col-12 col-lg-4 darker">
+            <h1>Nicholas Parr</h1>
+          </div>
+          <div className="col-12 col-lg-8 m-0">
+
+            <nav className="navbar navbar-expand-lg dark">
+
+              <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
+                data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <a className="nav-item nav-link active" href="index.html">About <span className="sr-only">(current)</span></a>
+                  <a className="nav-item nav-link" href="contact.html">Contact</a>
+                  <a className="nav-item nav-link" href="portfolio.html">Portfolio</a>
+
+                </div>
+              </div>
+            </nav>
+          </div>
+
+        </div>
+      </>
+    );
+  }
+}
+
+export default NavBar;
+
